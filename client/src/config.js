ced0417703ab0e81aeacbe5a329159d6ace9b267
Subrat Pandey <150380631+subratpandeyy@users.noreply.github.com>
2025-04-27 16:49:26 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -8,8 +8,4 @@ const config = {
   };
   
   const environment = process.env.NODE_ENV || 'development';
-<<<<<<< HEAD
   export default config[environment];
-=======
-  export default config[environment];
->>>>>>> 4c6174c4320e116f63106e18f9077b92e8e58771
